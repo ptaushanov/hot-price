04e605d23a309c79eb3cc3e7d23c8e1617d297c7
ptaushanov <pepi15032000@gmail.com>
2021-09-24 19:29:13 +0300

    feat: added 🔥 to all hot prices

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,7 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  document
+  .querySelectorAll(".price.hot")
+  .forEach(hotPrice => hotPrice.innerText += "🔥");
 });
